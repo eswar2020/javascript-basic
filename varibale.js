5f755677b4b59b85059ddff12d615b07abe81d8b
eswar <mullapudieswar20@gmail.com>
2018-07-17 18:14:31 +0530

    Add files via upload

---
+++ varibale.js
@@ -0,0 +1,19 @@
+let name = "eswar"
+let score = 100
+console.log(name)
+console.log(score)
+
+let firstname = "Mullapudi"
+let lastname ="Eswar"
+
+
+console.log(firstname + ' ' + lastname);
+
+let fullname = firstname + ' ' + lastname
+
+console.log(fullname)
+
+console.log(name + ' got score: ' + score)
+
+const total =100
+console.log( 'let the total be ' + total);
